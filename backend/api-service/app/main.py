c9f712d414b9b08b0b33e185adbd515740b91af2
Kuladeep Chowdary Ponnaganti <kponnaganti@evoketechnologies.com>
2026-09-07 20:16:51 +0530

    Update backend

--- backend/api-service/app/main.py
+++ backend/api-service/app/main.py
@@ -46,7 +46,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://ai-meeting-intelligence-silk.vercel.app/login"
+        "https://ai-meeting-intelligence-silk.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
